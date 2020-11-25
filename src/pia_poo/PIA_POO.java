adbaf4ad43948b7023f8df249782f26d9ce6b9ee
edgardo-vdz <67181328+edgardo-vdz@users.noreply.github.com>
2020-11-24 23:20:57 -0600

    Menu
    Se hizo el menu, pueden agregar mas opciones facilmente

--- src/pia_poo/PIA_POO.java
+++ src/pia_poo/PIA_POO.java
@@ -1,15 +1,37 @@
 
 package pia_poo;
 
+import java.util.Scanner;
+
 
 public class PIA_POO {
 
     
     public static void main(String[] args) {
+        
         Coordinador coordi = new Coordinador();
         Empleado emple = new Empleado();
-        coordi.registarCoordinadores(coordi);
-        emple.registarEmpleados(emple);
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("=== PROGRAMA DE GESTION DE PERSONAL ===");
+        System.out.println("1.-- REGISTRAR COORDINADORES");
+        System.out.println("2.-- REGISTRAR EMPLEADOS");
+        System.out.println("========================================");
+        System.out.println("Ingrese una opcion: > ");
+        int opcion = in.nextInt();
+        in.nextLine();
+        
+        switch(opcion){
+            case 1:
+                coordi.registarCoordinadores(coordi);
+                break;
+            case 2:
+                emple.registarEmpleados(emple);
+                break;
+            
+        }
+
     }
     
 }
