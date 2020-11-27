72f91249c393bf72e8410237a38e8c0ec20cdde1
Kevin-v-v <65782090+Kevin-v-v@users.noreply.github.com>
2020-11-27 11:30:03 -0600

    departamentos actualizados

--- src/pia_poo/IDepartamentos.java
+++ src/pia_poo/IDepartamentos.java
@@ -3,7 +3,6 @@ package pia_poo;
 
 
 public interface IDepartamentos {
-    static final String NOM_DEPA1 = "Jardin";
-    static final String NOM_DEPA2 = "Cocina";
+    static final String[] NOM_DEPTOS = {"Jardin","Cocina","Electrónica","Juguetería","Panadería"};
     //Y así se agregan los demas nombres de los departamentos para luego validar al ingresar datos de empleados CREEEO
 }
