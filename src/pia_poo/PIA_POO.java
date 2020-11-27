2170611a0a4a0b9f5efa8337a279de9a31281b72
Kevin-v-v <65782090+Kevin-v-v@users.noreply.github.com>
2020-11-27 11:46:54 -0600

    Cambio en el menu de inicio, opcion guardar y salir, arreglos de empleados y cordis añadidos

--- src/pia_poo/PIA_POO.java
+++ src/pia_poo/PIA_POO.java
@@ -9,28 +9,36 @@ public class PIA_POO {
     
     public static void main(String[] args) {
         
-        Coordinador coordi = new Coordinador();
-        Empleado emple = new Empleado();
-        
+        Coordinador[] coordinadores = new Coordinador[10];
+        Empleado[] empleados = new Empleado[100];
+        int contEmp = 0, contCordi = 0;
+        int opc;
         Scanner in = new Scanner(System.in);
-        
+        do{
+        do{
         System.out.println("=== PROGRAMA DE GESTION DE PERSONAL ===");
         System.out.println("1.-- REGISTRAR COORDINADORES");
         System.out.println("2.-- REGISTRAR EMPLEADOS");
+        System.out.println("3.-- GUARDAR Y SALIR");
         System.out.println("========================================");
         System.out.println("Ingrese una opcion: > ");
-        int opcion = in.nextInt();
+        opc = in.nextInt();
         in.nextLine();
+        }while(opc<1||opc>3);
         
-        switch(opcion){
+        switch(opc){
             case 1:
-                coordi.registarCoordinadores(coordi);
+                coordinadores[contCordi].registarCoordinadores(coordinadores[contCordi]);
                 break;
             case 2:
-                emple.registarEmpleados(emple);
+                empleados[contEmp].registarEmpleados(empleados[contEmp]);
+                break;
+            case 3:
+                //llama a la funcion de guardar y se termina la ejecucion
                 break;
             
         }
+        }while(opc!=3);
 
     }
     
